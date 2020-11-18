de444d659f6279e44210f30103252124a1d51389
GNRLies <3d.pegeot@laposte.net>
2020-11-18 20:10:49 +0100

    Script version plus récente

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 import subprocess;
+import time;
 
-serveur = subprocess.run(["./serveur", "6767"], capture_output=True);
-client1 = subprocess.run(["./client", "127.0.0.1 6767 test"], capture_output=True);
-client2 = subprocess.run(["./client", "127.0.0.1 6767 test2"], capture_output=True);
-print(serveur.stdout);
+print("Choisissez combien de fois vous souhaitez éxécutez les programmes")
+entree = input()
+n = int(entree)
+resultat = ""
+print("Choisissez le port de départ")
+p = input()
+po = int(p)
 
+for i in range (0,n) : 
+	port = str(po+i)
+	serveur = subprocess.Popen(["./serveur", port], stdout=subprocess.PIPE)
+	time.sleep(1)
+	client1 = subprocess.Popen(["./client", "127.0.0.1", port,  "test"], stdout=subprocess.DEVNULL)
+	client2 = subprocess.Popen(["./client", "127.0.0.1", port, "test2"], stdout=subprocess.DEVNULL)
+	sortie = serveur.communicate()[0]
+	resultat += str(sortie) + " \n"
+
+#print(resultat)
+f = open("resultat.txt", "w")
+f.write(resultat)
+f.close()
